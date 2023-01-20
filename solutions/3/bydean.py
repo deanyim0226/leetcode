2a506eee84f4653f8c36b7cd64432d02fc64eb7f
Dean Yim <donghyeon0226@gmail.com>
2023-01-19 18:50:28 -0800

    added 3 solution

---
+++ solutions/3/bydean.py
@@ -0,0 +1,19 @@
+def lengthOfLongestSubstring(s: str) -> int:
+
+    #initial approach to this problem would be using dictionary with associated indexes of string  
+    #if so, we can easily detect the duplicate character with o(1) time complexity
+    #whenever we found the repeating characters, we can calculate the length of the substring.
+     
+    longestLength = 0
+    prev = 0
+    dic = dict()
+
+    for curr, element in enumerate(s):
+
+        if element in dic: #the case where we find the duplicate character
+            prev = max(prev, dic[element]+1)
+        
+        dic[element] = curr #to indicate current index
+        longestLength = max(longestLength, curr-prev+1)
+
+    return longestLength
